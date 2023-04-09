c597bdf905659d6abb0624d67dc8094dd8fa92aa
emrhssla <emrhssla@gmail.com>
2023-04-09 23:43:54 +0900

    게시판 CRUD

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
+import PostCreateView from '@/views/posts/PostCreateView.vue';
+import PostDetailView from '@/views/posts/PostDetailView.vue';
+import PostEditView from '@/views/posts/PostEditView.vue';
+import PostListView from '@/views/posts/PostListView.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    component: AboutView,
+  },
+  {
+    path: '/posts',
+    component: PostListView,
+    name: 'PostList',
+  },
+  {
+    path: '/posts/create',
+    component: PostCreateView,
+    name: 'PostCreate',
+  },
+  {
+    path: '/posts/:id',
+    component: PostDetailView,
+    name: 'PostDetail',
+  },
+  {
+    path: '/posts/:id/edit',
+    component: PostEditView,
+    name: 'PostEdit',
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes: routes,
+});
+
+export default router;
